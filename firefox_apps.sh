37e14cb0064d3415576eacb2313297fa658b4140
Arthur Huan <arthur.huan@outlook.com>
2024-06-06 14:10:59 -0600

    Update firefox_apps.sh

--- firefox_apps.sh
+++ firefox_apps.sh
@@ -1,19 +1,25 @@
-#!/bin/bash    
-firefox -CreateProfile "$1"    
-touch "$HOME/.local/share/applications/$1.desktop"
-echo "[Desktop Entry]
-Version=1.0
-Type=Application
-Name=$1
-Exec=firefox --name=$1 --class $1 --no-remote -P $1 -url=$2
-StartupWMClass=$1
-Icon=$1
-" >> "$HOME/.local/share/applications/$1.desktop"
+#!/bin/bash
 
-cd $HOME/.mozilla/firefox/"$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed s/^Path=// | grep "$1")"
+# Parse the arguments, either `url name` or `url name displayName`
+if [[ $# -eq 2 ]]; then
+    url="$1"
+    name="$2"
+    displayName="$2"
+elif [[ $# -eq 3 ]]; then
+    url="$1"
+    name="$2"
+    displayName="$3"
+else
+    echo 'Invalid arguments.'
+fi
+
+# Create a new profile and customize
+firefox -CreateProfile $name
+cd $HOME/.mozilla/firefox/"$(grep 'Path=' ~/.mozilla/firefox/profiles.ini | sed s/^Path=// | grep "$name")"
 touch user.js
-echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> user.js
 echo 'user_pref("browser.tabs.inTitlebar", 0);' >> user.js
+# Manipulate userChrome to hide the tab bar
+echo 'user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);' >> user.js
 mkdir chrome
 cd chrome
 touch userChrome.css
@@ -21,15 +27,15 @@ echo "#TabsToolbar {visibility: collapse;}
 #navigator-toolbox {visibility: collapse;}
 " >> userChrome.css
 
-#!/bin/bash
+# Write respective .desktop file
+touch "$HOME/.local/share/applications/$name.desktop"
+echo "[Desktop Entry]
+Version=1.0
+Type=Application
+Name=$displayName
+Exec=firefox --name=$name --class $name --no-remote -P $name -url=$url
+StartupWMClass=$name
+Icon=$name 
+" >> "$HOME/.local/share/applications/$name.desktop"
 
-# Ask the user if they want to edit the .desktop file
-read -p "Do you want to edit the desktop file? [y/n] (n): " answer
-
-# Check if the answer is 'y' or 'Y'
-if [[ $answer = y ]] || [[ $answer = Y ]]
-then
-	${EDITOR:-vi} "$HOME/.local/share/applications/$1.desktop"
-else
-    echo "Aborted."
-fi
+echo "Process finished."
